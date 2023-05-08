a09fff5ad1f2eebf394972829801e23097361d04
EftekharAlam2 <hinsouvlmare@gmail.com>
2023-05-08 12:15:09 +0600

    layout added

---
+++ src/AddCoffe.jsx
@@ -0,0 +1,9 @@
+const AddCoffe = () => {
+  return (
+    <div>
+      <h1 className="bg-red-500">Coffee added</h1>
+    </div>
+  );
+};
+
+export default AddCoffe;
--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,24 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App";
+import AddCoffe from "./AddCoffe";
+import UpdateCoffee from "./UpdateCoffee";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <App></App>,
+    children: [
+      {
+        path: "/addCoffee",
+        element: <AddCoffe></AddCoffe>,
+      },
+      {
+        path: "/updatedCoffee",
+        element: <UpdateCoffee></UpdateCoffee>,
+      },
+    ],
   },
 ]);
 
